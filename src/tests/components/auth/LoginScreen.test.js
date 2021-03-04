ef88c55a80e1abb1afe764d6df5afe507e3f626b
Francisco Barrientos <francisco.barrientos@usach.cl>
2021-03-04 12:43:23 -0300

    Se agregan pruebas en LoginScreen

--- src/tests/components/auth/LoginScreen.test.js
+++ src/tests/components/auth/LoginScreen.test.js
@@ -8,6 +8,12 @@ import thunk from 'redux-thunk';
 import { MemoryRouter } from 'react-router-dom'
 
 import { LoginScreen } from "../../../components/auth/LoginScreen";
+import { startGoogleLogin, startLoginEmailPassword } from '../../../actions/auth';
+
+jest.mock('../../../actions/auth', () => ({
+    startGoogleLogin: jest.fn(),
+    startLoginEmailPassword: jest.fn()
+}));
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -21,6 +27,7 @@ const initState = {
 };
 
 let store = mockStore(initState);
+store.dispatch = jest.fn();
 
 const wrapper = mount(
     <Provider store={ store }>
@@ -34,10 +41,23 @@ describe('Pruebas en <LoginScreen />', () => {
 
     beforeEach( () => {
         store = mockStore(initState);
+        jest.clearAllMocks();
     });
     
     test('Debe de mostrarse correctamente', () => {
         expect( wrapper ).toMatchSnapshot();
+    });
+
+    test('Debe de disparar la acción de startGoogleLogin', () => {
+        wrapper.find('.google-btn').prop('onClick')();
+        expect( startGoogleLogin ).toHaveBeenCalled();
+    });
+
+    test('Debe de disparar el startLogin con los respectivos argumentos', () => {
+        wrapper.find('form').prop('onSubmit')({
+            preventDefault(){}
+        });
+        expect( startLoginEmailPassword ).toHaveBeenCalledWith('nando@gmail.com', '123456');
     });
     
 });
